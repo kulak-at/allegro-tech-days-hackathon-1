7edf8caf654764cf637d2085732b605c4c69e719
Kuba Mazurek <zakaprov@gmail.com>
2017-07-15 22:34:26 +0200

    Added view injection extensions

--- android-app/app/src/main/java/tech/allegro/allegrotechdays/activity/MapActivity.kt
+++ android-app/app/src/main/java/tech/allegro/allegrotechdays/activity/MapActivity.kt
@@ -1,10 +1,16 @@
 package tech.allegro.allegrotechdays.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
 import tech.allegro.allegrotechdays.R
+import tech.allegro.allegrotechdays.extensions.bindView
 
 class MapActivity : AppCompatActivity() {
+
+    val addReportButton: FloatingActionButton by bindView(R.id.map_fab_add_report)
+    val fragmentHolder: FrameLayout by bindView(R.id.map_fragment_holder)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
