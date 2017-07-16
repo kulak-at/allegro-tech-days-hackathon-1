a42eb47ead3e7d807925a48f2c4fded384911631
alyeen <whatanalyeen@gmail.com>
2017-07-16 11:35:43 +0200

    nearSphere query / bike-coords

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
+from __future__ import print_function
 from flask import Flask, abort, request, jsonify
+from bson.son import SON
 from datetime import datetime
 import json
+import sys
 from bson import json_util
 app = Flask(__name__)
-from pymongo import MongoClient
+from pymongo import MongoClient, GEOSPHERE
 client = MongoClient('mongodb://mongo:27017').bike_database
+client.coords.create_index([("location", GEOSPHERE)])
 userReportCollection = client.user_report
 bikeCoordCollection = client.coords
 
@@ -40,9 +44,20 @@ def getReports():
 
 @app.route('/bike-coords', methods=['GET'])
 def getBikeCoords():
-    coords = [coord for coord in bikeCoordCollection.find()]
+    params = request.args.to_dict()
+    lat = float(params.get('lat'))
+    lng = float(params.get('lng'))
+    rad = float(params.get('radius'))
+
+    query = {'location': {'$nearSphere': {'$geometry': {'type': 'Point', 'coordinates': [lng, lat]}, '$maxDistance': rad}}}
+
+    coords = []
+    if lat is None or lng is None or rad is None:
+        coords = [coord for coord in bikeCoordCollection.find()]
+    else:
+        coords = [coord for coord in bikeCoordCollection.find(query)]
     return json.dumps(coords, default=json_util.default)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
-
