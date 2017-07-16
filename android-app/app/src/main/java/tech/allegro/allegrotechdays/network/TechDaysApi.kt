ab15646c9f4073cab7d0f9598cec51e7bc7e04d5
Kuba Mazurek <zakaprov@gmail.com>
2017-07-16 11:57:39 +0200

    Created API definition and model for user report

--- android-app/app/src/main/java/tech/allegro/allegrotechdays/network/TechDaysApi.kt
+++ android-app/app/src/main/java/tech/allegro/allegrotechdays/network/TechDaysApi.kt
@@ -1,4 +1,13 @@
 package tech.allegro.allegrotechdays.network
 
+import io.reactivex.Observable
+import okhttp3.ResponseBody
+import retrofit2.http.Body
+import retrofit2.http.POST
+import tech.allegro.allegrotechdays.model.UserReport
+
 interface TechDaysApi {
+
+    @POST("/user-report")
+    fun getUserRepos(@Body report: UserReport): Observable<ResponseBody>
 }
